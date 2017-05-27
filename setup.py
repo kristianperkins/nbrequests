2ab8aff99a778f51fa08c050fae382a44cbf8d03
Kristian Perkins <khperkins@gmail.com>
2017-05-27 23:50:03 +1000

    some html formatting changes. handling different ways you can specify the request body.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='nbrequests',
     author='Kristian Perkins',
     author_email='khperkins@gmail.com',
-    version='0.0.5',
+    version='0.0.6',
     url='http://github.com/kristianperkins/nbrequests',
     py_modules=['nbrequests'],
     packages=['nbrequests'],
